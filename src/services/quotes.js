381cc904dcd336312e8912da46d823769b857300
Iri <16565764+irinelul@users.noreply.github.com>
2024-10-16 22:53:14 +0300

    move shit around

--- src/services/quotes.js
+++ src/services/quotes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3002/api'
+const baseUrl = '/api'
 
 const getAll = (searchTerm, page, limit) => {
     const request = axios.get(baseUrl, {
