60f5968145f5a73aed27b49db61095dd1c3f97cd
Iri <16565764+irinelul@users.noreply.github.com>
2024-10-16 22:53:14 +0300

    move shit around

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
 require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const Person = require('./models/mongodb');
+const app = express();
 
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const cors = require('cors')
-const Person = require('./models/mongodb')
+const PORT = process.env.PORT;
 
-app.use(express.static('dist'))
-app.use(cors())
-app.use(express.json())
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+app.use(cors());
+app.use(express.json());
+app.use(express.static('dist'));
 
 morgan.token('body', (req) => JSON.stringify(req.body));
-morgan.token('bodyLength', (req) =>  (JSON.stringify(req.body)).length) ;
-
-
-app.use(morgan(':method :url  status :status - :response-time ms content: :body :bodyLength Length  :res[header]'))
-
-
+morgan.token('bodyLength', (req) => (JSON.stringify(req.body)).length);
+app.use(morgan(':method :url  status :status - :response-time ms content: :body :bodyLength Length  :res[header]'));
 
 app.get('/api', (req, res) => {
     const searchTerm = req.query.searchTerm || '';
     const page = parseInt(req.query.page) || 1;
     const limit = 15;
     const skip = (page - 1) * limit;
+
     Person.find({ text: { $regex: searchTerm, $options: 'i' } })
         .skip(skip)
         .limit(limit)
@@ -49,7 +42,6 @@ app.get('/stats', async (req, res) => {
     try {
         const totalVideosPromise = Person.distinct('video_id').then(ids => ids.length);
         const totalQuotesPromise = Person.countDocuments({ text: { $ne: null } });
-
         const [totalVideos, totalQuotes] = await Promise.all([totalVideosPromise, totalQuotesPromise]);
 
         res.json({ totalVideos, totalQuotes });
@@ -58,34 +50,24 @@ app.get('/stats', async (req, res) => {
     }
 });
 
-
-
-app.get('/info', (request, response) => {
+app.get('/info', (req, res) => {
     Person.find({}).then(person => {
-        response.send(`Phonebook has info for ${person.length} persons <br>
-    Info is correct as of  ${new Date().toISOString()}`   )
-    })
-})
+        res.send(`Phonebook has info for ${person.length} persons <br> Info is correct as of  ${new Date().toISOString()}`);
+    });
+});
 
-
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message);
     if (error.name === 'CastError') {
-        console.error(error.message)
-
-        return response.status(400).send({ error: 'malformatted id' })
-
+        return res.status(400).send({ error: 'malformatted id' });
     } else if (error.name === 'ValidationError') {
-        console.log('phone number not correct type')
-        return response.status(400).json({ error: error.message })
+        return res.status(400).json({ error: error.message });
     }
-    // else if(error.name==='Person validation failed'){
-    //   console.log('phone number not correct type')
-    //   return response.status(400).json({ error:'phone number not correct type' })
-    // }
+    next(error);
+};
 
-    next(error)
-}
+app.use(errorHandler);
 
-app.use(errorHandler)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
