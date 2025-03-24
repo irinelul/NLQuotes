b2a75a8b50d07c5d603cfa9bc1715d71b2d0ed81
Iri <16565764+irinelul@users.noreply.github.com>
2025-03-24 17:39:12 +0200

    fix

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.use(express.static('dist', {
     setHeaders: (res, path) => {
         if (path.endsWith('.css')) {
             res.setHeader('Content-Type', 'text/css');
+        } else if (path.endsWith('.js')) {
+            res.setHeader('Content-Type', 'application/javascript');
         }
     }
 }));
