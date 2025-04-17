af1a3982fa369bcde5b060aca1f3fb30d4771b99
Jordan Weatherby <JordanLWeatherby@gmail.com>
2025-04-17 21:29:04 +0100

    update eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -9,8 +9,8 @@ export default [
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 'latest',
+      globals: {...globals.browser, ...globals.node},
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -29,6 +29,7 @@ export default [
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
+      'react/prop-types': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
