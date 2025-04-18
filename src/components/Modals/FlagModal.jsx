2f5a49989b77b1422a97403ea54c61b8b9f76d29
Jordan Weatherby <47519158+JordanWeatherby@users.noreply.github.com>
2025-04-18 11:58:58 +0100

    More small tidying up (#33)
    * remove unused svg
    
    * update eslint rules
    
    * more tidying
    
    * more tidying

--- src/components/Modals/FlagModal.jsx
+++ src/components/Modals/FlagModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styles from './Modals.module.css';
 
 export const FlagModal = ({ isOpen, onClose, onSubmit }) => {
